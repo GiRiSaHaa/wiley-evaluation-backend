e2034fa3d8349e5f3c8c3b7d226267529dea6f5c
GiRi SaHaa <giri91sa@gmailcom>
2021-06-04 09:14:32 +0530

    Initial commit

---
+++ src/test/java/com/wiley/assessment/main/WileyAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiley.assessment.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WileyAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
